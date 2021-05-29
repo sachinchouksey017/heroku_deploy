a12bbdc82bbcc67f947b3f620eea988ca2fd329a
sachinchouksey017 <sachinchouksey017@gmail.com>
2021-05-29 18:15:17 +0530

    Done with server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var bodyParser = require('body-parser');
 var path = require('path')
-// require('')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname + '/quantity-measurement')));
+app.use(express.static(path.join(__dirname + '/dist/')));
 app.get('/*', function (req, res) {
-
-    res.send('<h1>hello</h1>').status(200);
+    res.sendFile(path.join(__dirname + '/'))
 });
 var listener = server.listen(process.env.PORT || 5000, function () {
-    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+    console.log(`Listening on port ${listener.address().port}`); //Listening on port 5000
 });
